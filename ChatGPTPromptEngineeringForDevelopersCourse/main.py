d38a64bb46081060ba290fb79257641d29667da2
Boyang Tian <boyangtian@Boyangs-MacBook-Pro-M1.local>
2023-05-15 17:20:03 -0600

    lesson 2

---
+++ ChatGPTPromptEngineeringForDevelopersCourse/main.py
@@ -0,0 +1,18 @@
+import openai
+import os
+
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv())
+
+openai.api_key  = os.getenv('OPENAI_API_KEY')
+
+GPTMODEL="gpt-3.5-turbo"
+
+def get_completion(prompt, model=GPTMODEL):
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=0, # this is the degree of randomness of the model's output
+    )
+    return response.choices[0].message["content"]
