943dbce60f2f9a000768e4fda0e0c91e00195dfa
Boyang Tian <boyangtian@boyangs-mbp-m1.lan>
2023-06-19 20:44:25 -0700

    add post method, redirect and race condition

--- SimpleDjangoWebApp/simplesite/polls/views.py
+++ SimpleDjangoWebApp/simplesite/polls/views.py
@@ -1,8 +1,9 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.template import loader
+from django.urls import reverse
 
-from .models import Question
+from .models import Choice, Question
 
 # Create your views here.
 def index(request):
@@ -30,8 +31,31 @@ def detail(request, question_id):
     return render(request, "polls/detail.html", {"question": question})
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
+    else:
+        # race condition: https://docs.djangoproject.com/en/4.2/ref/models/expressions/#avoiding-race-conditions-using-f
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        # This function helps avoid having to hardcode a URL in the view function. 
+        # It is given the name of the view that we want to pass control to and the variable portion
+        # of the URL pattern that points to that view.
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
