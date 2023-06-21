d0798963a427e8d419f4a1e83717fc846bf8c32f
Boyang Tian <boyangtian@boyangs-mbp-m1.lan>
2023-06-20 20:26:10 -0700

    each time create object also need to create related object which has a foreign key point to this

--- SimpleDjangoWebApp/simplesite/polls/admin.py
+++ SimpleDjangoWebApp/simplesite/polls/admin.py
@@ -3,6 +3,15 @@ from django.contrib import admin
 # Register your models here.
 from .models import Choice, Question
 
+# in this way it tells Django "Choice" object are edited on the "Question" admin page.
+# By default, provide enough fileds for 3 choices
+# StackedInline will show 3 fileds one by one
+# class ChoiceInline(admin.StackedInline):
+# TabularInline will show a tabular way of displaying inline related objects
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
 # customize admin page
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
@@ -10,6 +19,6 @@ class QuestionAdmin(admin.ModelAdmin):
         (None, {"fields": ["question_text"]}),
         ("Date information", {"fields": ["pub_date"]}),
     ]
+    inlines = [ChoiceInline]
 
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)
