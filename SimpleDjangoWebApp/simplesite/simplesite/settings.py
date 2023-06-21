f282c6204b35a69409a0aa194f7d51c7a03d3a3e
BoyangTian <boyangtiancs@gmail.com>
2023-06-20 17:05:01 -0700

    [FullStack][Django] ccs add static support and image support. Move template and static folders to root (#17)
    * add static support for ccs
    
    * move static under root
    
    * move template to root folder
    
    * add image
    
    https://docs.djangoproject.com/en/4.2/intro/tutorial06/
    
    ---------
    
    Co-authored-by: Boyang Tian <boyangtian@boyangs-mbp-m1.lan>

--- SimpleDjangoWebApp/simplesite/simplesite/settings.py
+++ SimpleDjangoWebApp/simplesite/simplesite/settings.py
@@ -55,11 +55,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'simplesite.urls'
 
+# use specific template folder instead of template file under each app
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),],
+        'APP_DIRS': False,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
@@ -88,7 +89,6 @@ DATABASES = {
         "TIME_ZONE": "UTC",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -125,6 +125,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# https://docs.djangoproject.com/en/4.2/intro/tutorial06/
+# Django’s STATICFILES_FINDERS setting contains a list of finders that know how to discover static
+# files from various sources. One of the defaults is AppDirectoriesFinder which looks for a “static”
+# subdirectory in each of the INSTALLED_APPS, like the one in polls we just created.
+# The admin site uses the same directory structure for its static files.
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
