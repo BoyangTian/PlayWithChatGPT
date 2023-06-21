e1863a8c6eaffe5580a4322a5cbcac6a497051df
Boyang Tian <boyangtian@boyangs-mbp-m1.lan>
2023-06-20 17:03:46 -0700

    add image

--- SimpleDjangoWebApp/simplesite/simplesite/settings.py
+++ SimpleDjangoWebApp/simplesite/simplesite/settings.py
@@ -90,20 +90,6 @@ DATABASES = {
     }
 }
 
-
-# https://docs.djangoproject.com/en/4.2/intro/tutorial06/
-# Django’s STATICFILES_FINDERS setting contains a list of finders that know how to discover static
-# files from various sources. One of the defaults is AppDirectoriesFinder which looks for a “static”
-# subdirectory in each of the INSTALLED_APPS, like the one in polls we just created.
-# The admin site uses the same directory structure for its static files.
-STATICFILES_FINDERS = [
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-]
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
@@ -139,6 +125,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# https://docs.djangoproject.com/en/4.2/intro/tutorial06/
+# Django’s STATICFILES_FINDERS setting contains a list of finders that know how to discover static
+# files from various sources. One of the defaults is AppDirectoriesFinder which looks for a “static”
+# subdirectory in each of the INSTALLED_APPS, like the one in polls we just created.
+# The admin site uses the same directory structure for its static files.
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
