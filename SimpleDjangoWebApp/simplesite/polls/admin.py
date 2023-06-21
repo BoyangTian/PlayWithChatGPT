f4a5203e734cc5ddac3de0185a339dfa14085638
BoyangTian <boyangtiancs@gmail.com>
2023-06-20 20:46:25 -0700

    [FullStack][Django] fix admin page and Customize "admin" page by add more column, sort, search and html (#18)
    * fix admin page and customize admin page
    
    * each time create object also need to create related object which has a foreign key point to this
    
    * add sort and search ability for admin page
    
    * add admin html
    
    https://docs.djangoproject.com/en/4.2/intro/tutorial07/
    
    ---------
    
    Co-authored-by: Boyang Tian <boyangtian@boyangs-mbp-m1.lan>

--- SimpleDjangoWebApp/simplesite/polls/admin.py
+++ SimpleDjangoWebApp/simplesite/polls/admin.py
@@ -3,5 +3,31 @@ from django.contrib import admin
 # Register your models here.
 from .models import Choice, Question
 
-admin.site.register(Question)
-admin.site.register(Choice)
+# in this way it tells Django "Choice" object are edited on the "Question" admin page.
+# By default, provide enough fileds for 3 choices
+# StackedInline will show 3 fileds one by one
+# class ChoiceInline(admin.StackedInline):
+# TabularInline will show a tabular way of displaying inline related objects
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+# customize admin page
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        # The first element of each tuple in fieldsets is the title of the fieldset.
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"]}),
+    ]
+    # show all field under home display place
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+    # enable "admin" page adds a “Filter” sidebar that lets people filter the change list by the pub_date field
+    list_filter = ["pub_date"]
+    inlines = [ChoiceInline]
+    # enable search capability
+    # because it uses a LIKE query behind the scenes,
+    # limiting the number of search fields to a reasonable number will make it easier
+    # for your database to do the search
+    search_fields = ["question_text"]
+
+admin.site.register(Question, QuestionAdmin)
--- SimpleDjangoWebApp/simplesite/polls/models.py
+++ SimpleDjangoWebApp/simplesite/polls/models.py
@@ -1,5 +1,6 @@
 import datetime
 
+from django.contrib import admin
 from django.db import models
 from django.utils import timezone
 
@@ -11,6 +12,12 @@ class Question(models.Model):
     def __str__(self):
         return "id: " + str(self.id) + ", " + self.question_text
     
+    # enable "admin" page sort this column
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
