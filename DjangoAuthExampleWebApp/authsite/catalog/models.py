1fdd8e96fd8acc6c4a1209a0fec1f65d3fb8d195
Boyang Tian <boyangtian@boyangs-mbp-m1.lan>
2023-06-22 21:56:32 -0700

    create model

--- DjangoAuthExampleWebApp/authsite/catalog/models.py
+++ DjangoAuthExampleWebApp/authsite/catalog/models.py
@@ -1,3 +1,101 @@
+import uuid # Required for unique book instances
+
 from django.db import models
+from django.urls import reverse # Used to generate URLs by reversing the URL patterns
 
 # Create your models here.
+# good link of the relationships of the model below
+# https://developer.mozilla.org/en-US/docs/Learn/Server-side/Django/Models
+
+class Genre(models.Model):
+    """Model representing a book genre."""
+    name = models.CharField(max_length=200, help_text='Enter a book genre (e.g. Science Fiction)')
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.name
+
+class Book(models.Model):
+    """Model representing a book (but not a specific copy of a book)."""
+    title = models.CharField(max_length=200)
+
+    # Foreign Key used because book can only have one author, but authors can have multiple books
+    # Author is a string rather than an object because it hasn't been declared yet in the file
+    # null=True, which allows the database to store a Null value if no author is selected, 
+    # and on_delete=models.SET_NULL, which will set the value of the book's author field to Null 
+    # if the associated author record is deleted.
+    # Warning: By default on_delete=models.CASCADE, which means that if the author was deleted, 
+    # this book would be deleted too! We use SET_NULL here, but we could also use PROTECT or RESTRICT 
+    # to prevent the author being deleted while any book uses it.
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+
+    summary = models.TextField(max_length=1000, help_text='Enter a brief description of the book')
+    isbn = models.CharField('ISBN', max_length=13, unique=True,
+                             help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
+
+    # ManyToManyField used because genre can contain many books. Books can cover many genres.
+    # Genre class has already been defined so we can specify the object above.
+    genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.title
+
+    def get_absolute_url(self):
+        """Returns the URL to access a detail record for this book."""
+        return reverse('book-detail', args=[str(self.id)])
+
+class BookInstance(models.Model):
+    """Model representing a specific copy of a book (i.e. that can be borrowed from the library)."""
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for this particular book across whole library')
+    book = models.ForeignKey('Book', on_delete=models.RESTRICT, null=True)
+    imprint = models.CharField(max_length=200)
+    due_back = models.DateField(null=True, blank=True)
+
+    LOAN_STATUS = (
+        ('m', 'Maintenance'),
+        ('o', 'On loan'),
+        ('a', 'Available'),
+        ('r', 'Reserved'),
+    )
+
+    status = models.CharField(
+        max_length=1,
+        choices=LOAN_STATUS,
+        blank=True,
+        default='m',
+        help_text='Book availability',
+    )
+
+    # declare model-level metadata for your Model
+    # One of the most useful features of this metadata is to control the default ordering of 
+    # records returned when you query the model type. You do this by specifying the match order in 
+    # a list of field names to the ordering attribute, as shown above. The ordering will depend on 
+    # the type of field (character fields are sorted alphabetically, while date fields are sorted 
+    # in chronological order). You can prefix the field name with a minus symbol (-) to reverse the 
+    # sorting order.
+    class Meta:
+        ordering = ['due_back']
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return f'{self.id} ({self.book.title})'
+    
+
+class Author(models.Model):
+    """Model representing an author."""
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField('Died', null=True, blank=True)
+
+    class Meta:
+        ordering = ['last_name', 'first_name']
+
+    def get_absolute_url(self):
+        """Returns the URL to access a particular author instance."""
+        return reverse('author-detail', args=[str(self.id)])
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return f'{self.last_name}, {self.first_name}'
