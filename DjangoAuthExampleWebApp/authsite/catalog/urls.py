dc1d75547fdaf5411ea10bf10c54e06e25d45bd5
Boyang Tian <boyangtian@boyangs-mbp-m1.lan>
2023-06-23 17:36:36 -0700

    add more pages

--- DjangoAuthExampleWebApp/authsite/catalog/urls.py
+++ DjangoAuthExampleWebApp/authsite/catalog/urls.py
@@ -5,6 +5,11 @@ from . import views
 # Since we move the "template" folder out the current folder, we need to change html path
 app_name = "catalog"
 
+# regex find path can achieve by "re_path" method:
+# https://developer.mozilla.org/en-US/docs/Learn/Server-side/Django/Generic_views
+
 urlpatterns = [
     path("", views.index, name="index"),
+    path('books/', views.BookListView.as_view(), name='books'),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
 ]
--- DjangoAuthExampleWebApp/authsite/catalog/views.py
+++ DjangoAuthExampleWebApp/authsite/catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
 
 from .models import Book, Author, BookInstance, Genre
 
@@ -26,3 +27,36 @@ def index(request):
     # Since we move the "template" folder out the current folder, we need to change html path
     # return render(request, 'index.html', context=context)
     return render(request, 'catalog/index.html', context=context)
+
+class BookListView(generic.ListView):
+    # Only this one line under "generic.ListView" will query the database to get all records for the 
+    # specified model (Book) then render a template located at book_list.html
+    # the generic views look for templates in /application_name/the_model_name_list.html 
+    # (catalog/book_list.html in this case) inside the application's /application_name/templates/ 
+    # directory (/catalog/templates/). You can overwrite by "template_name" attribute
+    model = Book
+    context_object_name = 'book_list'
+    template_name = 'books/book_list.html' 
+    # queryset = Book.objects.filter(title__icontains='war')[:5] # Get 5 books containing the title war
+    # def get_queryset(self):
+    #     return Book.objects.filter(title__icontains='war')[:5] # Get 5 books containing the title war
+    queryset = Book.objects.all()
+
+    paginate_by = 2
+
+    # def get_context_data(self, **kwargs):
+    #     # Call the base implementation first to get the context
+    #     context = super(BookListView, self).get_context_data(**kwargs)
+    #     # Create any data and add it to the context
+    #     context['some_data'] = 'This is just some data'
+    #     return context
+
+class BookDetailView(generic.DetailView):
+    model = Book
+    # this is the object name pass to html file
+    # context_object_name = 'book-detail'
+    template_name = 'catalog/book_detail.html' 
+
+    paginate_by = 2
+
+
